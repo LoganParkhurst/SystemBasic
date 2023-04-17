cf7d21e6063c88dbc7b98a64b3463f983d9f2ca3
LoganParkhurst <parkhurst@colum.edu>
2023-04-17 12:38:33 -0500

    Add project files.

---
+++ SystemBasic/Cave.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemBasic
+{
+    public class Cave
+    {
+        string name = "Bracken Cave";
+        int batPopulation = 0;
+        int wormPopulation = 0;
+        int hawkPopulation = 0;
+        int beetlePopulation = 0;
+        int amountOfCotton = 0;
+        int amountOfCorn = 0;
+
+        public void HarvestCotton(Player player)
+        {
+
+        }
+
+        public void HarvestCorn(Player player)
+        {
+
+        }
+    }
+}
---
+++ SystemBasic/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemBasic
+{
+    public class Player
+    {
+        string name;
+        int personalAmountOfCorn = 0;
+        int personalAmountOfCotton = 0;
+    }
+}
